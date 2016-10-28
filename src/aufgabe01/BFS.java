1c40908bcb591d40befb4ab3cb23c5316a0e59bf
Stefan Staegemann <stefanstaegemann@hotmail.de>
2016-10-28 13:51:17 +0200

    main

--- src/aufgabe01/BFS.java
+++ src/aufgabe01/BFS.java
@@ -1,7 +1,46 @@
 package aufgabe01;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import org.jgrapht.DirectedGraph;
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+
 public class BFS {
 	
-	
+	public BFS() {
+		DirectedGraph<String, DefaultEdge> g = new DefaultDirectedGraph<String, DefaultEdge>(DefaultEdge.class);
+		
+	}
 
+	
+	
+	
+	public int bfs(Graph<String, DefaultEdge> graph, String start, String ziel) {
+
+		Queue<String> queue = new LinkedList<String>();
+		String[] s = graph.edgesOf(start).toArray(new String[0]);
+		List<String> bearbeitet = new ArrayList<>();
+
+		if (queue.contains(ziel)) {
+			return 1;
+		} else {
+
+			for (int i = 0; i < s.length; i++) {
+				if (!queue.contains(s[0])) {
+					queue.add(s[i]);
+				}
+			}
+
+			if (queue.size() > 0) {
+				return 1 + bfs(graph, queue.remove(), ziel);
+			} else {
+				return 0;
+			}
+		}
+	}
 }
--- src/aufgabe01/Graph.java
+++ src/aufgabe01/Graph.java
@@ -2,4 +2,8 @@ package aufgabe01;
 
 public class Graph {
 
+	
+	public static void main(String[] args) {
+		
+	}
 }
